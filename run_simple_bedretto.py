ecbda9d32efaaef35d9adf5294a2670c0145422a
Jakub Both <jakub.both@uib.no>
2025-04-01 09:46:44 +0000

    integrate functionality for line search (working) and return map (not working)

--- run_simple_bedretto.py
+++ run_simple_bedretto.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 import numpy as np
 import porepy as pp
+from porepy.numerics.nonlinear import line_search
 from simple_bedretto.geometry import BedrettoGeometry
 from common.numerics import (
     DarcysLawAd,
@@ -26,10 +27,10 @@ from common.statistics import AdvancedSolverStatistics, LogPerformanceDataVector
 
 from common.contact_mechanics import (
     LinearRadialReturnTangentialContact,
-    ScaledContact,
-    NCPNormalContact,
-    NCPTangentialContact,
+    NCPContact,
 )
+
+from common.newton_return_map import NewtonReturnMap
 
 
 # Set logging level
@@ -37,7 +38,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-class ScaledRadialReturnModel(
+class NonlinearRadialReturnModel(
     BedrettoGeometry,  # Geometry
     AuxiliaryContact,  # Yield function, orthognality, and alignment
     FractureStates,  # Physics based contact states for output only
@@ -46,26 +47,21 @@ class ScaledRadialReturnModel(
     LogPerformanceDataVectorial,  # Tailored convergence checks
     ReverseElasticModuli,  # Characteristic displacement from traction
     Physics,  # Basic model, BC and IC
-):
-    """Mixed-dimensional poroelastic problem."""
+): """Simple Bedretto model solved with Huebers nonlinear radial return formulation."""
 
 
-class ScaledLinearRadialReturnModel(
-    LinearRadialReturnTangentialContact, ScaledRadialReturnModel
-): ...
+class LinearRadialReturnModel(
+    LinearRadialReturnTangentialContact, NonlinearRadialReturnModel
+): """Simple Bedretto model solved with Alart linear radial return formulation."""
+
+class NCPModel(
+    NCPContact,
+    NonlinearRadialReturnModel,
+): """Simple Bedretto model solved with NCP formulation."""
 
 
-# NCP Formulations
-class ScaledNCPModel(
-    ScaledContact,
-    NCPNormalContact,
-    NCPTangentialContact,
-    ScaledRadialReturnModel,
-): ...
-
-
-def generate_case_name(ad_mode, mode):
-    return f"{ad_mode}_{mode}"
+def generate_case_name(ad_mode, formulation):
+    return f"{ad_mode}_{formulation}"
 
 
 if __name__ == "__main__":
@@ -76,14 +72,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run simple Bedretto case.")
     parser.add_argument("--ad-mode", type=str, default="picard", help="AD mode.")
     parser.add_argument(
-        "--mode", type=str, default="rr-nonlinear", help="Formulation to use."
+        "--formulation", type=str, default="rr-nonlinear", help="Formulation to use."
     )
     parser.add_argument(
         "--num-fractures", type=int, default=6, help="Number of fractures."
     )
     args = parser.parse_args()
     ad_mode = args.ad_mode
-    mode = args.mode
+    formulation = args.formulation
 
     # Model parameters
     model_params = {
@@ -111,7 +107,7 @@ if __name__ == "__main__":
         "linear_solver": "scipy_sparse",
         "max_iterations": 200,  # Needed for export
         "folder_name": Path("visualization/simple_bedretto")
-        / generate_case_name(ad_mode, mode),
+        / generate_case_name(ad_mode, formulation),
         "nonlinear_solver_statistics": AdvancedSolverStatistics,
     }
     Path(model_params["folder_name"]).mkdir(parents=True, exist_ok=True)
@@ -136,47 +132,45 @@ if __name__ == "__main__":
 
         case "newton":
 
-            class ScaledNCPModel(DarcysLawAd, ScaledNCPModel): ...
+            class NonlinearRadialReturnModel(DarcysLawAd, NonlinearRadialReturnModel): ...
+
+            class LinearRadialReturnModel(
+                DarcysLawAd, LinearRadialReturnModel
+            ): ...
 
             class NCPModel(DarcysLawAd, NCPModel): ...
-
-            class ScaledRadialReturnModel(DarcysLawAd, ScaledRadialReturnModel): ...
-
-            class ScaledLinearRadialReturnModel(
-                DarcysLawAd, ScaledLinearRadialReturnModel
-            ): ...
 
         case _:
             raise ValueError(f"AD mode {ad_mode} not recognized.")
 
     # Model setup
     logger.info(f"\n\nRunning {model_params['folder_name']}")
-    if mode == "rr-nonlinear":
-        model = ScaledRadialReturnModel(model_params)
+    if formulation == "rr-nonlinear":
+        model = NonlinearRadialReturnModel(model_params)
 
-    elif mode == "rr-linear":
-        model = ScaledLinearRadialReturnModel(model_params)
+    elif formulation == "rr-linear":
+        model = LinearRadialReturnModel(model_params)
 
-    elif mode == "ncp-min":
+    elif formulation == "ncp-min":
         model_params["ncp_type"] = "min"
-        model = ScaledNCPModel(model_params)
+        model = NCPModel(model_params)
 
-    elif mode == "ncp-fb":
+    elif formulation == "ncp-fb":
         model_params["ncp_type"] = "fb"
-        model = ScaledNCPModel(model_params)
+        model = NCPModel(model_params)
 
-    elif mode == "ncp-fb-full":
+    elif formulation == "ncp-fb-full":
         model_params["ncp_type"] = "fb-full"
-        model = ScaledNCPModel(model_params)
+        model = NCPModel(model_params)
 
-    elif mode == "rr-nonlinear-linesearch":
+    elif formulation == "rr-nonlinear-linesearch":
         
-        class ScaledRadialReturnModel(
+        class NonlinearRadialReturnModel(
             pp.models.solution_strategy.ContactIndicators,
-            ScaledRadialReturnModel,
+            NonlinearRadialReturnModel,
         ):
             """Added contact indicators for line search."""
-        model = ScaledRadialReturnModel(model_params)
+        model = NonlinearRadialReturnModel(model_params)
 
         class ConstraintLineSearchNonlinearSolver(
             line_search.ConstraintLineSearch,  # The tailoring to contact constraints.
@@ -195,8 +189,18 @@ if __name__ == "__main__":
             1  # Scale the indicator adaptively to increase robustness
         )
 
+    elif formulation == "rr-nonlinear-return-map":
+        
+        class NonlinearRadialReturnModel(
+            NewtonReturnMap,
+            NonlinearRadialReturnModel,
+        ):
+            """Add return map before each iteration."""
+
+        model = NonlinearRadialReturnModel(model_params)
+
     else:
-        raise ValueError(f"Mode {mode} not recognized.")
+        raise ValueError(f"formulation {formulation} not recognized.")
 
     pp.run_time_dependent_model(model, solver_params)
 
