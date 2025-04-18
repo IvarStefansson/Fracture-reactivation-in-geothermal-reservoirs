031fe3996085f884ad94a1318411b18abf6cdf42
Jakub Both <jakub.both@uib.no>
2025-04-18 10:31:28 +0000

    add second set of parameters - mostly literature values

--- simple_bedretto/physics.py
+++ simple_bedretto/physics.py
@@ -1,47 +1,89 @@
-from dataclasses import dataclass
-from typing import Callable, ClassVar
-from warnings import warn
-
 import numpy as np
 import porepy as pp
-from porepy.models.fluid_mass_balance import FluidMassBalanceEquations
+import egc
 
-# ! ---- MATERIAL PARAMETERS ----
+if False:
+    # Used so far but based on previous works by PMG
 
-fluid_parameters: dict[str, float] = {
-    "compressibility": 0,
-    "viscosity": 1e-3,
-    "density": 998.2e0,
-}
+    # ! ---- MATERIAL PARAMETERS ----
 
-solid_parameters: dict[str, float] = {
-    "biot_coefficient": 1.0,
-    "permeability": 1e-14,
-    "normal_permeability": 1e-14,
-    "porosity": 1.0e-2,
-    "shear_modulus": 1e14,
-    "lame_lambda": 1e14,
-    "residual_aperture": 1e-3,
-    "density": 2600,
-    "maximum_elastic_fracture_opening": 0e-3,  # Not used
-    "fracture_normal_stiffness": 1e3,  # Not used
-    "fracture_tangential_stiffness": -1,
-    "fracture_gap": 0e-3,  # Equals the maximum fracture closure.
-    "dilation_angle": 0.1,
-    "friction_coefficient": 0.8,
-}
+    fluid_parameters: dict[str, float] = {
+        "compressibility": 0,
+        "viscosity": 1e-3,
+        "density": 998.2e0,
+    }
 
-injection_schedule = {
-    "time": [pp.DAY, 2 * pp.DAY] + [(3 + i) * pp.DAY for i in range(5)],
-    "pressure": [0, 0] + [3e7, 5e7, 10e7, 5e7, 5e7],
-    "reference_pressure": 1e7,
-}
+    solid_parameters: dict[str, float] = {
+        "biot_coefficient": 1.0,
+        "permeability": 1e-14,
+        "normal_permeability": 1e-14,
+        "porosity": 1.0e-2,
+        "shear_modulus": 1e14,
+        "lame_lambda": 1e14,
+        "residual_aperture": 1e-3,
+        "density": 2600,
+        "maximum_elastic_fracture_opening": 0e-3,  # Not used
+        "fracture_normal_stiffness": 1e3,  # Not used
+        "fracture_tangential_stiffness": -1,
+        "fracture_gap": 0e-3,  # Equals the maximum fracture closure.
+        "dilation_angle": 0.1,
+        "friction_coefficient": 0.8,
+    }
 
-numerics_parameters: dict[str, float] = {
-    "open_state_tolerance": 1e-10,  # Numerical method parameter
-    "characteristic_contact_traction": injection_schedule["reference_pressure"],
-    "contact_mechanics_scaling": 1.0,
-}
+    injection_schedule = {
+        "time": [pp.DAY, 2 * pp.DAY] + [(3 + i) * pp.DAY for i in range(5)],
+        "pressure": [0, 0] + [3e7, 5e7, 10e7, 5e7, 5e7],
+        "reference_pressure": 1e7,
+    }
+
+    numerics_parameters: dict[str, float] = {
+        "open_state_tolerance": 1e-10,  # Numerical method parameter
+        "characteristic_contact_traction": injection_schedule["reference_pressure"],
+        "contact_mechanics_scaling": 1.0,
+    }
+
+else:
+    # Based on publications on BedrettoLab - a much harder problem
+
+    # ! ---- MATERIAL PARAMETERS ----
+
+    fluid_parameters: dict[str, float] = {
+        "compressibility": 0,  # 4.6e-10, # 25 deg C
+        "viscosity": 0.89e-3,  # 25 deg C
+        "density": 998.2e0,
+    }
+
+    # Values from Multi-disciplinary characterizations of the BedrettoLab
+    solid_parameters: dict[str, float] = {
+        # Guessed
+        "biot_coefficient": 1,  # guessed by Vaezi et al.
+        "normal_permeability": 4.35e-6 * pp.DARCY,  # guessed
+        "dilation_angle": 0.1,  # guessed
+        # Literature values
+        "permeability": 4.35e-6 * pp.DARCY,  # X.Ma et al.
+        "residual_aperture": 1e-4,  # Computed from transmissivities (X. Ma et al.) and cubic law
+        "porosity": 1.36e-2,  # X.Ma et al.
+        "shear_modulus": 16.8e9,  # X.Ma et al.
+        "lame_lambda": 60.5e9 - 2 / 3 * 16.8e9,  # X.Ma et al.
+        "density": 2653,  # X.Ma et al.
+        "fracture_gap": 0e-3,  # Equals the maximum fracture closure.
+        "friction_coefficient": 0.6,  # X.Ma et al.
+        "maximum_elastic_fracture_opening": 0e-3,  # Not used
+        "fracture_normal_stiffness": 1e3,  # Not used
+        "fracture_tangential_stiffness": -1,  # Not used
+    }
+
+    injection_schedule = {
+        "time": [pp.DAY, 2 * pp.DAY] + [(3 + i) * pp.DAY for i in range(5)],
+        "pressure": [0, 0] + [3e7, 5e7, 10e7, 5e7, 5e7],
+        "reference_pressure": 1e7,
+    }
+
+    numerics_parameters: dict[str, float] = {
+        "open_state_tolerance": 1e-10,  # Numerical method parameter
+        "characteristic_contact_traction": injection_schedule["reference_pressure"],
+        "contact_mechanics_scaling": 1.0,
+    }
 
 
 class HorizontalBackgroundStress(egc.BackgroundStress):
