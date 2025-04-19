2c3d36e5461bbfc584173e6955228c2b9784780b
Jakub Both <jakub.both@uib.no>
2025-04-19 18:36:26 +0000

    restructure return map and make it more robust

--- src/egc/newton_return_map.py
+++ src/egc/newton_return_map.py
@@ -1,104 +1,98 @@
-import numpy as np
-import porepy as pp
+"""Newton return map for contact tractions as preprocessing step.
 
-# Add a postprocessing step after every nonlinear iteration that functions
-# as a return map for the contact forces. If the tangential traction is
-# larger in magnitude than the friction bound (whose value depends on
-# the current guess of the normal traction), it is projected back to the
-# boundary. Similarly, if the normal traction is larger than zero, it is
-# projected back to the "boundary" of zero.
+Add a preprocessing step before every nonlinear iteration that functions
+as a return map for the contact forces. If the tangential traction is
+larger in magnitude than the friction bound (whose value depends on
+the current guess of the normal traction), it is projected back to the
+boundary. Similarly, if the normal traction is larger than zero, it is
+projected back to the "boundary" of zero.
+
+"""
+
+import numpy as np
 
 
 class NewtonReturnMap:
-    # Note: We perform the return map before the nonlinear iteration (i.e. after the previous iteration)
-    # since we want the convergence check to be done on the regular Newton update. It is not done
-    # before the very first iteration.
+    """Preprocessing step for the Newton solver to perform a return map.
+
+    We perform the return map before the nonlinear iteration, since we want
+    the convergence check to be done on the regular Newton update. It is not
+    done before the very first iteration.
+
+    """
+
     def before_nonlinear_iteration(self) -> None:
         if self.nonlinear_solver_statistics.num_iteration > 0:
             # Evaluate tractions and the friction bound.
             fracture_domains = self.mdg.subdomains(dim=self.nd - 1)
 
-            normal_basis = self.normal_component(fracture_domains)
-            tangential_basis = self.tangential_component(fracture_domains)
-
-            t_t = self.tangential_component(fracture_domains) @ self.contact_traction(
-                fracture_domains
-            )
+            # Evaluate normal traction.
             t_n = self.normal_component(fracture_domains) @ self.contact_traction(
                 fracture_domains
             )
-            t_t_eval = t_t.value(self.equation_system)
             t_n_eval = t_n.value(self.equation_system)
 
-            # This will be our updated traction array, based on the results of the
-            # return map.
-            t_eval_new = np.zeros(self.contact_traction(fracture_domains).size)
+            # Return map in normal direction.
+            t_n_new = np.clip(t_n_eval, None, 0.0)
 
-            # Find the indices of the global traction array corresponding to the normal and
-            # tangential components. This is equivalent to the column position of all nonzero elements
-            # of the normal and tangential projection matrices, respectively.
+            # Put normal in the right positions of the global traction array.
+            # Projection matrix (represented as an ArraySlicer) from the full
+            # traction to the normal direction. The prolongation operator is
+            # the transpose
+            normal_basis = self.normal_component(fracture_domains)
+            normal_restriction = normal_basis._slicer
+            normal_prolongation = normal_restriction.T
+            t_eval_new = normal_prolongation @ t_n_new
 
-            # print(self.tangential_component(fracture_domains).value(self.equation_system))
-
-            # assert False
-            # tang_matrix = self.tangential_component(fracture_domains).value(self.equation_system)
-            # norm_matrix = self.normal_component(fracture_domains).value(self.equation_system)
-            # print(np.nonzero(tang_matrix))
-            # print(np.nonzero(norm_matrix))
-            # tang_indices = np.nonzero(tang_matrix)[1]
-            # norm_indices = np.nonzero(norm_matrix)[1]
-
-            # If the simulation is three-dimensional, the tangential component
-            # is (cell-wise) two-dimensional and is accordingly put into
-            # a two-dimensional array.
+            # Evaluate tangential traction and split into its components.
+            t_t = self.tangential_component(fracture_domains) @ self.contact_traction(
+                fracture_domains
+            )
+            t_t_eval = t_t.value(self.equation_system)
             if self.nd == 3:
-                t_n_new = t_n_eval
-                t_t_new = t_t_eval.reshape((int(t_t_eval.size / 2), 2))
+                t_t_new = t_t_eval.reshape((-1, 2))
             else:
-                t_n_new = t_n_eval
                 t_t_new = t_t_eval
 
-            # Return map in normal direction.
-            for i in range(len(t_n_new)):
-                if t_n_new[i] > 0:
-                    t_n_new[i] = 0  # Set the normal traction to zero.
+            # Return map in tangential direction, using the newly obtained normal
+            # tractions.
             friction_coeff = self.friction_coefficient(fracture_domains).value(
                 self.equation_system
             )
-            friction_bound = -1.0 * friction_coeff * t_n_new
-            # Return map in tangential direction, using the newly
-            # obtained normal tractions.
-            for i, (bound, t_t) in enumerate(zip(friction_bound, t_t_new)):
-                if np.linalg.norm(t_t) > bound:
-                    # Tangential traction exceeds friction bound,
-                    # project back to the boundary.
-                    t_t_new[i] = bound * t_t / np.linalg.norm(t_t)
+            friction_bound = -friction_coeff * t_n_new
 
-            # Put normal and tangential tractions in the right positions of the global
-            # traction array.
+            # Use a trust radius to limit the tangential traction - by default, use
+            # the physical bounds, but could be set to a larger value to allow for
+            # relaxation.
+            trust_radius = 1.0 * friction_bound
 
-            # Projection matrix (represented as an ArraySlicer) from the full traction
-            # to the normal direction.
-            normal_restriction = normal_basis._slicer
-            # Take the transpose to map back to the full traction space.
-            normal_prolongation = normal_restriction.T
+            # To avoid numerical issues, we set the tangential traction explicitly
+            # to zero if the normal traction is zero. This follows the same principle
+            # as in the definition of the tangential fracture deformation equation.
+            char_closed = friction_bound > self.numerical.open_state_tolerance
+            t_t_new[char_closed] = (
+                np.minimum(
+                    1.0,
+                    trust_radius[char_closed]
+                    / np.linalg.norm(t_t_new[char_closed], axis=1),
+                ).reshape((-1, 1))
+                * t_t_new[char_closed]
+            )
+            t_t_new[~char_closed] = 0.0
 
-            # The tangential projection is essentially a list of Ad projection
-            # operators. These are stored as the children (this was the simplest way to
-            # get the Ad parsing machinery to collaborate).
+            # Put tangential tractions in the right positions of the global traction
+            # array. The tangential projection is essentially a list of Ad projection
+            # operators. These are stored as the children (this was the simplest way
+            # to get the Ad parsing machinery to collaborate).
+            tangential_basis = self.tangential_component(fracture_domains)
             tangential_restriction = tangential_basis.children
 
-            # To get the transporse, we fetch the slicer of each child and take its
+            # To get the transpose, we fetch the slicer of each child and take its
             # transpose. This should leave tangential_prolongation as a list of
-            # ArraySlicers.
+            # ArraySlicers. Again, a apply the projection operator to the tangential
+            # traction.
             tangential_prolongation = [e._slicer.T for e in tangential_restriction]
-
-            # Apply the normal projection operator to the normal traction.
-            t_eval_new = normal_prolongation @ t_n_new
-            # Loop over the tangential projection operators and apply them to the
-            # components of the tangential traction.
             for ind, e_i in enumerate(tangential_prolongation):
-                # Apply the projection operator to the tangential traction.
                 t_eval_new += e_i @ t_t_new[:, ind]
 
             # Update traction values after having performed the return map.
