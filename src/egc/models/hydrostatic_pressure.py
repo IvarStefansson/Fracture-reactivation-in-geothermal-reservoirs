2b617c00b970db7c4121aee21a32505480c87453
Jakub Both <jakub.both@uib.no>
2025-04-22 07:37:09 +0000

    minor refactoring of hydrostatic pressure

--- src/egc/models/hydrostatic_pressure.py
+++ src/egc/models/hydrostatic_pressure.py
@@ -4,12 +4,20 @@ import numpy as np
 from porepy.models.fluid_mass_balance import FluidMassBalanceEquations
 import egc
 
-class HydrostaticPressureBC:
-    """Hydrostatic pressure boundary condition active on all sides of the domain."""
 
-    domain_boundary_sides: Callable[[pp.Grid | pp.BoundaryGrid], pp.domain.DomainSides]
-    is_well: Callable[[pp.Grid], bool]
+class HydrostaticPressure:
+    """Utility class to compute (generalized) hydrostatic pressure."""
+
     fluid: pp.FluidComponent
+
+    def hydrostatic_pressure(self, sd: pp.Grid) -> np.ndarray:
+        p_atm = 0
+        gravity = self.units.convert_units(pp.GRAVITY_ACCELERATION, "m*s^-2")
+        fluid_density = self.fluid.density([sd]).value(self.equation_system)
+        rho_g = fluid_density * gravity
+        z = sd.cell_centers[self.nd - 1]
+        pressure = p_atm - rho_g * z
+        return pressure
 
     def update_time_dependent_ad_arrays(self) -> None:
         """Set hydrostatic pressure for current gravity."""
@@ -25,14 +33,16 @@ class HydrostaticPressureBC:
                 iterate_index=0,
             )
 
+
+class HydrostaticPressureBC(HydrostaticPressure):
+    """Hydrostatic pressure boundary condition active on all sides of the domain."""
+
+    domain_boundary_sides: Callable[[pp.Grid | pp.BoundaryGrid], pp.domain.DomainSides]
+
     def _fluid_pressure_boundary_faces(self, sd: pp.Grid) -> np.ndarray:
         """Auxiliary method to identify all Dirichlet/pressure boundaries."""
-        if sd.dim == self.nd:
-            domain_sides = self.domain_boundary_sides(sd)
-            faces = domain_sides.all_bf
-        else:
-            faces = np.array([], dtype=int)
-        return faces
+        domain_sides = self.domain_boundary_sides(sd)
+        return domain_sides.all_bf
 
     def bc_type_darcy_flux(self, sd: pp.Grid) -> pp.BoundaryCondition:
         return pp.BoundaryCondition(sd, self._fluid_pressure_boundary_faces(sd), "dir")
@@ -43,15 +53,6 @@ class HydrostaticPressureBC:
     @property
     def onset(self) -> bool:
         return self.time_manager.time > self.time_manager.schedule[0] + 1e-5
-
-    def hydrostatic_pressure(self, sd: pp.Grid) -> np.ndarray:
-        p_atm = 0
-        gravity = self.units.convert_units(pp.GRAVITY_ACCELERATION, "m*s^-2")
-        fluid_density = self.fluid.density([sd]).value(self.equation_system)
-        rho_g = fluid_density * gravity
-        z = sd.cell_centers[self.nd-1]
-        pressure = p_atm - rho_g * z
-        return pressure
 
     def bc_values_pressure(self, boundary_grid: pp.BoundaryGrid) -> np.ndarray:
         pressure = np.zeros(boundary_grid.num_cells)
@@ -65,7 +66,8 @@ class HydrostaticPressureBC:
 
         return pressure
 
-class HydrostaticPressureInitialCondition(egc.InitialCondition):
+
+class HydrostaticPressureInitialCondition(egc.InitialCondition, HydrostaticPressure):
     """Hydrostatic pressure based on cell coordinates as initial condition for the pressure."""
 
     def initial_pressure(self, sd=None):
@@ -76,25 +78,30 @@ class HydrostaticPressureInitialCondition(egc.InitialCondition):
                 [self.hydrostatic_pressure(sd[i]) for i in range(len(sd))]
             )
 
-class HydrostaticPressureInitialization:
+
+class HydrostaticPressureInitialization(HydrostaticPressure):
     """Fix pressure to be the hydrostatic pressure for the first day."""
 
     def update_time_dependent_ad_arrays(self) -> None:
-        """Set hydrostatic pressure for current gravity."""
+        """Deactivate pressure equations and set hydrostatic pressure for current gravity."""
+
         super().update_time_dependent_ad_arrays()
 
         # Update injection pressure
         for sd in self.mdg.subdomains(return_data=False):
-            
+            active_indicator = float(self.time_manager.time < 1 * pp.DAY + 1e-5)
             pp.set_solution_values(
                 name="pressure_constraint_indicator",
-                values=(self.time_manager.time < 1 * pp.DAY + 1e-5)
-                * np.ones(sd.num_cells, dtype=float),
+                values=active_indicator * np.ones(sd.num_cells, dtype=float),
                 data=self.mdg.subdomain_data(sd),
                 iterate_index=0,
             )
 
     def mass_balance_equation(self, subdomains: list[pp.Grid]) -> pp.ad.Operator:
+        """Overwrites mass balance equation to set the pressure equal to the hydrostatic pressure."""
+
+        eq = super().mass_balance_equation(subdomains)
+
         hydrostatic_pressure = pp.ad.TimeDependentDenseArray(
             "hydrostatic_pressure", subdomains
         )
@@ -105,9 +112,7 @@ class HydrostaticPressureInitialization:
             "pressure_constraint_indicator", subdomains
         )
 
-        eq = super().mass_balance_equation(subdomains)
-
-        combined_eq = indicator * constrained_eq + (pp.ad.Scalar(1.0) - indicator) * eq
+        combined_eq = eq + indicator * (constrained_eq - eq)
         # YZ: Iterative linear solver relies on this name to find this equation.
         combined_eq.set_name(FluidMassBalanceEquations.primary_equation_name())
         return combined_eq
