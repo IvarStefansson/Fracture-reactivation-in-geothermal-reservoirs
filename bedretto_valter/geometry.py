9e5b8cd2146c8ac77074b097ffb7bec0f615200d
Jakub Both <jakub.both@uib.no>
2025-04-27 03:35:41 +0000

    deactivate some fractures, adjust box

--- bedretto_valter/geometry.py
+++ bedretto_valter/geometry.py
@@ -17,14 +17,13 @@ from porepy.applications.md_grids.model_geometries import CubeDomainOrthogonalFr
 
 class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
     def set_domain(self) -> None:
-        
         bounding_box = {
-            "xmin": self.units.convert_units(-200, "m"),
-            "xmax": self.units.convert_units(200, "m"),
-            "ymin": self.units.convert_units(-200, "m"),
-            "ymax": self.units.convert_units(200, "m"),
+            "xmin": self.units.convert_units(-500, "m"),
+            "xmax": self.units.convert_units(500, "m"),
+            "ymin": self.units.convert_units(-500, "m"),
+            "ymax": self.units.convert_units(500, "m"),
             "zmin": self.units.convert_units(-2000, "m"),
-            "zmax": self.units.convert_units(0, "m"),
+            "zmax": self.units.convert_units(-1000, "m"),
         }
         self._domain = pp.Domain(bounding_box)
 
@@ -43,72 +42,75 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
 
     def cb1(self, md) -> np.ndarray:
         """TM: tunnel depth in m. (x,y)=(0,0) corresponds to the lab."""
-        xyz = np.array([0,0, self.units.convert_units(-1000-md, "m")])
+        xyz = np.array([0, 0, self.units.convert_units(-1000 - md, "m")])
         return xyz
 
     def set_fractures(self) -> None:
-
         # Interval 14
-        interval_14 = [pp.create_elliptic_fracture(
-            center=self.cb1(90),
-            major_axis=self.units.convert_units(50, "m"),
-            minor_axis=self.units.convert_units(50, "m"),
-            major_axis_angle=0,  # TODO?
-            strike_angle=237 * np.pi / 180,  # E-W
-            dip_angle=63 * np.pi / 180,
-            num_points=16,
-            # index=0,
-        )
+        interval_14 = [
+            pp.create_elliptic_fracture(
+                center=self.cb1(90),
+                major_axis=self.units.convert_units(50, "m"),
+                minor_axis=self.units.convert_units(50, "m"),
+                major_axis_angle=0,  # TODO?
+                strike_angle=237 * np.pi / 180,  # E-W
+                dip_angle=63 * np.pi / 180,
+                num_points=16,
+                # index=0,
+            )
         ]
+        pressurized_14 = len(interval_14) * [False]
 
         # Interval 13
         interval_13 = [
             pp.create_elliptic_fracture(
                 center=self.cb1(110),
-                major_axis=self.units.convert_units(150, "m"),
-                minor_axis=self.units.convert_units(150, "m"),
+                major_axis=self.units.convert_units(50, "m"),
+                minor_axis=self.units.convert_units(50, "m"),
                 major_axis_angle=0,  # TODO?
-                strike_angle= 240 * np.pi / 180,  # E-W
+                strike_angle=240 * np.pi / 180,  # E-W
                 dip_angle=69 * np.pi / 180,
                 num_points=16,
                 # index=0,
             ),
         ]
+        pressurized_13 = len(interval_13) * [False]
 
         # Interval 12
         interval_12 = [
-            pp.create_elliptic_fracture(
-                center=self.cb1(131),
-                major_axis=self.units.convert_units(50, "m"),
-                minor_axis=self.units.convert_units(50, "m"),
-                major_axis_angle=0,  # TODO?
-                strike_angle= 231 * np.pi / 180,  # E-W
-                dip_angle=50 * np.pi / 180,
-                num_points=16,
-                # index=0,
-            ),
-            pp.create_elliptic_fracture(
-                center=self.cb1(133),
-                major_axis=self.units.convert_units(50, "m"),
-                minor_axis=self.units.convert_units(50, "m"),
-                major_axis_angle=0,  # TODO?
-                strike_angle= 241 * np.pi / 180,  # E-W
-                dip_angle=65 * np.pi / 180,
-                num_points=16,
-                # index=0,
-            ),
-            pp.create_elliptic_fracture(
-                center=self.cb1(135),
-                major_axis=self.units.convert_units(50, "m"),
-                minor_axis=self.units.convert_units(50, "m"),
-                major_axis_angle=0,  # TODO?
-                strike_angle= 251 * np.pi / 180,  # E-W
-                dip_angle=80 * np.pi / 180,
-                num_points=16,
-                # index=0,
-            ),
+            # NOTE: The interesting one!
+            # pp.create_elliptic_fracture(
+            #    center=self.cb1(131),
+            #    major_axis=self.units.convert_units(50, "m"),
+            #    minor_axis=self.units.convert_units(50, "m"),
+            #    major_axis_angle=0,  # TODO?
+            #    strike_angle= 231 * np.pi / 180,  # E-W
+            #    dip_angle=50 * np.pi / 180,
+            #    num_points=16,
+            #    # index=0,
+            # ),
+            # pp.create_elliptic_fracture(
+            #    center=self.cb1(133),
+            #    major_axis=self.units.convert_units(50, "m"),
+            #    minor_axis=self.units.convert_units(50, "m"),
+            #    major_axis_angle=0,  # TODO?
+            #    strike_angle= 241 * np.pi / 180,  # E-W
+            #    dip_angle=65 * np.pi / 180,
+            #    num_points=16,
+            #    # index=0,
+            # ),
+            # pp.create_elliptic_fracture(
+            #    center=self.cb1(135),
+            #    major_axis=self.units.convert_units(50, "m"),
+            #    minor_axis=self.units.convert_units(50, "m"),
+            #    major_axis_angle=0,  # TODO?
+            #    strike_angle= 251 * np.pi / 180,  # E-W
+            #    dip_angle=80 * np.pi / 180,
+            #    num_points=16,
+            #    # index=0,
+            # ),
         ]
-                
+        pressurized_12 = len(interval_12) * [False]
 
         # Interval 11
         interval_11 = [
@@ -123,6 +125,7 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
                 # index=0,
             ),
         ]
+        pressurized_11 = len(interval_11) * [False]
 
         # Interval 10
         interval_10 = [
@@ -137,6 +140,7 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
                 # index=0,
             ),
         ]
+        pressurized_10 = len(interval_10) * [False]
 
         # Interval 9
         # TODO many more?
@@ -152,6 +156,7 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
                 # index=0,
             ),
         ]
+        pressurized_9 = len(interval_9) * [True]
 
         # Interval 8
         interval_8 = [
@@ -166,9 +171,9 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
                 # index=0,
             ),
         ]
+        pressurized_8 = len(interval_8) * [False]
 
         # Interval 7
-        # TODO add second fracture zone?
         interval_7 = [
             pp.create_elliptic_fracture(
                 center=self.cb1(219),
@@ -180,17 +185,36 @@ class BedrettoValter_Geometry(CubeDomainOrthogonalFractures):
                 num_points=16,
                 # index=0,
             ),
-            pp.create_elliptic_fracture(
-                center=self.cb1(221),
-                major_axis=self.units.convert_units(50, "m"),
-                minor_axis=self.units.convert_units(50, "m"),
-                major_axis_angle=0,  # TODO?
-                strike_angle=240 * np.pi / 180,  # N-S
-                dip_angle=60 * np.pi / 180,
-                num_points=16,
-                # index=0,
-            ),
+            # pp.create_elliptic_fracture(
+            #    center=self.cb1(221),
+            #    major_axis=self.units.convert_units(50, "m"),
+            #    minor_axis=self.units.convert_units(50, "m"),
+            #    major_axis_angle=0,  # TODO?
+            #    strike_angle=240 * np.pi / 180,  # N-S
+            #    dip_angle=60 * np.pi / 180,
+            #    num_points=16,
+            #    # index=0,
+            # ),
         ]
+        pressurized_7 = len(interval_7) * [False]
 
-
-        self._fractures = interval_14 + interval_13 + interval_12 + interval_11 + interval_10 + interval_9 + interval_8 + interval_7 
+        self._fractures = (
+            interval_14
+            + interval_13
+            + interval_12
+            + interval_11
+            + interval_10
+            + interval_9
+            + interval_8
+            + interval_7
+        )
+        self.pressurized_fractures = np.where(
+            pressurized_14
+            + pressurized_13
+            + pressurized_12
+            + pressurized_11
+            + pressurized_10
+            + pressurized_9
+            + pressurized_8
+            + pressurized_7
+        )[0]
